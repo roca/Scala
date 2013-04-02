e69fb3641573a2d73ef2cfa14219f9672eea2008
roca <romelcampbell@gmail.com>
2013-04-02 17:41:28 -0400

    x

--- workspace/progfun/.worksheet/src/week1.session.scala
+++ workspace/progfun/.worksheet/src/week1.session.scala
@@ -1,7 +1,7 @@
 package week1
 
-object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(37); val res$0 = 
-  1+3;System.out.println("""res0: Int(4) = """ + $show(res$0));$skip(42); 
+object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(38); val res$0 = 
+   1+3;System.out.println("""res0: Int(4) = """ + $show(res$0));$skip(42); 
 	def abs(x:Double) = if (x < 0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(313); 
 	
 	def sqrt(x: Double) = {
@@ -22,5 +22,6 @@ object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport.
 	sqrt(2);System.out.println("""res1: Double = """ + $show(res$1));$skip(9); val res$2 = 
 	sqrt(4);System.out.println("""res2: Double = """ + $show(res$2));$skip(12); val res$3 = 
 	sqrt(1e-6);System.out.println("""res3: Double = """ + $show(res$3));$skip(12); val res$4 = 
-	sqrt(1e60);System.out.println("""res4: Double = """ + $show(res$4))}
+	sqrt(1e60);System.out.println("""res4: Double = """ + $show(res$4));$skip(10); val res$5 = 
+  sqrt(5);System.out.println("""res5: Double = """ + $show(res$5))}
 }
---
+++ workspace/progfun/.worksheet/src/week2.currying.scala
@@ -0,0 +1,5 @@
+package week2
+
+object currying {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(76); 
+  println("Welcome to the Scala worksheet")}
+}
