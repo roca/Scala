e4c8233cdc1e38ae0b30a562445f6d7cf23618a5
roca <romelcampbell@gmail.com>
2013-04-05 14:43:46 -0400

    Done with week 2

--- workspace/progfun/.worksheet/src/week2.rationals.scala
+++ workspace/progfun/.worksheet/src/week2.rationals.scala
@@ -8,14 +8,14 @@ object rationals {;import org.scalaide.worksheet.runtime.library.WorksheetSuppor
   
   val y = new Rational(5,7);System.out.println("""y  : week2.Rational = """ + $show(y ));$skip(31); 
   
-  val z = new Rational(3,2);System.out.println("""z  : week2.Rational = """ + $show(z ));$skip(14); val res$2 = 
+  val z = new Rational(3,2);System.out.println("""z  : week2.Rational = """ + $show(z ));$skip(11); val res$2 = 
   
-  x.add(y);System.out.println("""res2: week2.Rational = """ + $show(res$2));$skip(18); val res$3 = 
-  x.sub(y).sub(z);System.out.println("""res3: week2.Rational = """ + $show(res$3));$skip(11); val res$4 = 
-  y.add(y);System.out.println("""res4: week2.Rational = """ + $show(res$4));$skip(12); val res$5 = 
-  x.less(y);System.out.println("""res5: Boolean = """ + $show(res$5));$skip(11); val res$6 = 
-  x.max(y);System.out.println("""res6: week2.Rational = """ + $show(res$6));$skip(11); val res$7 = 
-  x.min(y);System.out.println("""res7: week2.Rational = """ + $show(res$7));$skip(29); 
+  x + y;System.out.println("""res2: week2.Rational = """ + $show(res$2));$skip(12); val res$3 = 
+  x - y - z;System.out.println("""res3: week2.Rational = """ + $show(res$3));$skip(8); val res$4 = 
+  y + y;System.out.println("""res4: week2.Rational = """ + $show(res$4));$skip(8); val res$5 = 
+  x < y;System.out.println("""res5: Boolean = """ + $show(res$5));$skip(10); val res$6 = 
+  x max y;System.out.println("""res6: week2.Rational = """ + $show(res$6));$skip(10); val res$7 = 
+  x min y;System.out.println("""res7: week2.Rational = """ + $show(res$7));$skip(29); 
   
   val t = new Rational(2);System.out.println("""t  : week2.Rational = """ + $show(t ))}
  }
@@ -29,13 +29,13 @@ class Rational(x:Int, y:Int) {
     
     def this(x:Int) = this(x,1)
     
-    def less(that: Rational): Boolean = {this.numer * that.denom < that.numer * this.denom}
+    def < (that: Rational): Boolean = {this.numer * that.denom < that.numer * this.denom}
     
-    def max(that: Rational): Rational =  {if (this.less(that)) that else this}
+    def max(that: Rational): Rational =  {if (this < that) that else this}
 
-    def min(that: Rational): Rational =  {if (this.less(that)) this else that}
+    def min(that: Rational): Rational =  {if (this < that) this else that}
     
-    def add(that: Rational) : Rational = {
+    def + (that: Rational) : Rational = {
       new Rational(this.numer * that.denom + that.numer * this.denom, this.denom * that.denom)
     }
     
@@ -43,12 +43,12 @@ class Rational(x:Int, y:Int) {
      this.numer  + "/" + this.denom
     }
     
-    def neg() : Rational = {
+    def unary_- : Rational = {
       new Rational(-this.numer,this.denom)
     }
     
-    def sub(that: Rational): Rational = {
+    def - (that: Rational): Rational = {
       //new Rational(this.numer * that.denom - that.numer * this.denom, this.denom * that.denom)
-      add(that.neg)
+      this + -that
     }
  }
