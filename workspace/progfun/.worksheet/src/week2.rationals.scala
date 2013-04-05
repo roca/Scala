6ad519be58bd4f5a107c95a64f68484f823c8c73
roca <romelcampbell@gmail.com>
2013-04-05 12:14:35 -0400

    Rational part #2

--- workspace/progfun/.worksheet/src/week2.rationals.scala
+++ workspace/progfun/.worksheet/src/week2.rationals.scala
@@ -11,27 +11,44 @@ object rationals {;import org.scalaide.worksheet.runtime.library.WorksheetSuppor
   val z = new Rational(3,2);System.out.println("""z  : week2.Rational = """ + $show(z ));$skip(14); val res$2 = 
   
   x.add(y);System.out.println("""res2: week2.Rational = """ + $show(res$2));$skip(18); val res$3 = 
-  x.sub(y).sub(z);System.out.println("""res3: week2.Rational = """ + $show(res$3))}
-}
+  x.sub(y).sub(z);System.out.println("""res3: week2.Rational = """ + $show(res$3));$skip(11); val res$4 = 
+  y.add(y);System.out.println("""res4: week2.Rational = """ + $show(res$4));$skip(12); val res$5 = 
+  x.less(y);System.out.println("""res5: Boolean = """ + $show(res$5));$skip(11); val res$6 = 
+  x.max(y);System.out.println("""res6: week2.Rational = """ + $show(res$6));$skip(11); val res$7 = 
+  x.min(y);System.out.println("""res7: week2.Rational = """ + $show(res$7));$skip(29); 
+  
+  val t = new Rational(2);System.out.println("""t  : week2.Rational = """ + $show(t ))}
+ }
 
 class Rational(x:Int, y:Int) {
-    def numer = x
-    def denom = y
+    require( y != 0 , "denominator must be nonzero")
+    private def gcd(a:Int, b:Int): Int = {if (b == 0) a else gcd(b, a % b)}
+    private val g = gcd(x,y)
+    def numer = x / g
+    def denom = y / g
     
-    def add(r: Rational) : Rational = {
-      new Rational(numer * r.denom + r.numer * denom, denom * r.denom)
+    def this(x:Int) = this(x,1)
+    
+    def less(that: Rational): Boolean = {this.numer * that.denom < that.numer * this.denom}
+    
+    def max(that: Rational): Rational =  {if (this.less(that)) that else this}
+
+    def min(that: Rational): Rational =  {if (this.less(that)) this else that}
+    
+    def add(that: Rational) : Rational = {
+      new Rational(this.numer * that.denom + that.numer * this.denom, this.denom * that.denom)
     }
     
     override def toString(): String = {
-     numer  + "/" + denom
+     this.numer  + "/" + this.denom
     }
     
     def neg() : Rational = {
-      new Rational(-numer,denom)
+      new Rational(-this.numer,this.denom)
     }
     
-    def sub(r: Rational): Rational = {
-      //new Rational(numer * r.denom - r.numer * denom, denom * r.denom)
-      add(r.neg)
+    def sub(that: Rational): Rational = {
+      //new Rational(this.numer * that.denom - that.numer * this.denom, this.denom * that.denom)
+      add(that.neg)
     }
  }
--- workspace/progfun/src/week2/rationals.sc
+++ workspace/progfun/src/week2/rationals.sc
@@ -12,26 +12,43 @@ object rationals {
   
   x.add(y)                                        //> res2: week2.Rational = 22/21
   x.sub(y).sub(z)                                 //> res3: week2.Rational = -79/42
-}
+  y.add(y)                                        //> res4: week2.Rational = 10/7
+  x.less(y)                                       //> res5: Boolean = true
+  x.max(y)                                        //> res6: week2.Rational = 5/7
+  x.min(y)                                        //> res7: week2.Rational = 1/3
+  
+  val t = new Rational(2)                         //> t  : week2.Rational = 2/1
+ }
 
 class Rational(x:Int, y:Int) {
-    def numer = x
-    def denom = y
+    require( y != 0 , "denominator must be nonzero")
+    private def gcd(a:Int, b:Int): Int = {if (b == 0) a else gcd(b, a % b)}
+    private val g = gcd(x,y)
+    def numer = x / g
+    def denom = y / g
     
-    def add(r: Rational) : Rational = {
-      new Rational(numer * r.denom + r.numer * denom, denom * r.denom)
+    def this(x:Int) = this(x,1)
+    
+    def less(that: Rational): Boolean = {this.numer * that.denom < that.numer * this.denom}
+    
+    def max(that: Rational): Rational =  {if (this.less(that)) that else this}
+
+    def min(that: Rational): Rational =  {if (this.less(that)) this else that}
+    
+    def add(that: Rational) : Rational = {
+      new Rational(this.numer * that.denom + that.numer * this.denom, this.denom * that.denom)
     }
     
     override def toString(): String = {
-     numer  + "/" + denom
+     this.numer  + "/" + this.denom
     }
     
     def neg() : Rational = {
-      new Rational(-numer,denom)
+      new Rational(-this.numer,this.denom)
     }
     
-    def sub(r: Rational): Rational = {
-      //new Rational(numer * r.denom - r.numer * denom, denom * r.denom)
-      add(r.neg)
+    def sub(that: Rational): Rational = {
+      //new Rational(this.numer * that.denom - that.numer * this.denom, this.denom * that.denom)
+      add(that.neg)
     }
  }
