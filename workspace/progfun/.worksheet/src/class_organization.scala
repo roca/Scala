e55e3315ed79fa89d34a529eee4de68344a6ba52
roca <romelcampbell@gmail.com>
2013-04-10 17:14:08 -0400

    Week3.2 code

--- workspace/progfun/.worksheet/src/class_organization.scala
+++ workspace/progfun/.worksheet/src/class_organization.scala
@@ -2,5 +2,12 @@
 import week3.Rational
 
 object class_organization {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(72); val res$0 = 
-  new  Rational(1,2);System.out.println("""res0: week3.Rational = """ + $show(res$0))}
+  new  Rational(1,2);System.out.println("""res0: week3.Rational = """ + $show(res$0));$skip(51); 
+  
+  def error(msg: String) = throw new Error(msg);System.out.println("""error: (msg: String)Nothing""");$skip(18); 
+  
+  val x = null;System.out.println("""x  : Null = """ + $show(x ));$skip(20); 
+  val y: String = x;System.out.println("""y  : String = """ + $show(y ));$skip(29); val res$1 = 
+  
+   if (true) 1 else false;System.out.println("""res1: AnyVal = """ + $show(res$1))}
 }
