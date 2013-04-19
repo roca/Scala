8748c4b667a600c3f1a1aa1cd318a400c6ee65e0
roca <romelcampbell@gmail.com>
2013-04-19 11:37:03 -0400

    Installed aptana studio plugin

---
+++ workspace/.metadata/.plugins/com.aptana.ruby.core/-545838435/4/object.rb
@@ -0,0 +1,142 @@
+class Object < BasicObject
+  include Kernel
+
+  ARGF = ARGF
+  ARGV = []
+  ArgumentError = ArgumentError
+  Array = Array
+  BasicObject = BasicObject
+  Bignum = Bignum
+  Binding = Binding
+  Class = Class
+  Comparable = Comparable
+  Complex = Complex
+  Data = Data
+  Dir = Dir
+  ENV = {"ECLIPSE_HOME"=>"/Applications/eclipse/Eclipse.app/Contents/MacOS", "TERM"=>"xterm-color", "JAVA_STARTED_ON_FIRST_THREAD_16615"=>"1", "DYLD_LIBRARY_PATH"=>"/usr/local/oracle/11.2/client64/lib/", "JAVA_HOME"=>"/System/Library/Frameworks/JavaVM.framework/Home", "APP_ICON_16615"=>"../Resources/Eclipse.icns", "rvm_version"=>"1.16.11 (stable)", "TERM_PROGRAM"=>"Apple_Terminal", "PWD"=>"/Applications/eclipse/Eclipse.app/Contents/MacOS", "PS1"=>"\\[\\033]0;\\w\\007\\] [\\e[0;32m\\]\\@\\e[0m\\]]\\e[1;33m\\]\\h:\\e[0m\\] \\e[0;32m\\]\\w >\\e[0m\\]", "APP_ICON_16799"=>"../Resources/Eclipse.icns", "MY_RUBY_HOME"=>"/Users/romel.campbell/.rvm/rubies/ruby-1.9.2-p320", "TMPDIR"=>"/var/folders/vk/5c079xk576qgg28f28hnz58jf8nh0x/T/", "PATH"=>"/Users/romel.campbell/.rvm/gems/ruby-1.9.2-p320/bin:/Users/romel.campbell/.rvm/gems/ruby-1.9.2-p320@global/bin:/Users/romel.campbell/.rvm/rubies/ruby-1.9.2-p320/bin:/Users/romel.campbell/.rvm/bin:/System/Library/Frameworks/JavaVM.framework/Home/bin:/usr/local/bin:/Users/romel.campbell/bin:/usr/local/mysql/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/Users/romel.campbell/.rvm/gems/ruby-1.9.2-p320/bin:/Users/romel.campbell/.rvm/gems/ruby-1.9.2-p320@global/bin:/Users/romel.campbell/.rvm/rubies/ruby-1.9.2-p320/bin:/Users/romel.campbell/.rvm/bin:/System/Library/Frameworks/JavaVM.framework/Home/bin:/Users/romel.campbell/bin:/usr/local/mysql/bin:/usr/local/bin/:/usr/local/oracle/11.2/client64/bin:/Applications/eclipse/Eclipse.app/Contents/MacOS:/Applications/play-2.1.0:/usr/local/bin/:/usr/local/oracle/11.2/client64/bin:/Applications/eclipse/Eclipse.app/Contents/MacOS:/Applications/play-2.1.0", "APTANA_VERSION"=>"3.4.0.1358388620", "PLAY_HOME"=>"/Applications/play-2.1.0", "rvm_env_string"=>"ruby-1.9.2-p320", "JAVA_STARTED_ON_FIRST_THREAD_16799"=>"1", "rvm_path"=>"/Users/romel.campbell/.rvm", "Apple_PubSub_Socket_Render"=>"/tmp/launch-jBWiW3/Render", "rvm_ruby_string"=>"ruby-1.9.2-p320", "NLS_LANG"=>"AMERICAN_AMERICA.UTF8", "GEM_PATH"=>"/Users/romel.campbell/.rvm/gems/ruby-1.9.2-p320:/Users/romel.campbell/.rvm/gems/ruby-1.9.2-p320@global", "ORACLE_HOME"=>"/usr/local/oracle/11.2/client64", "com.apple.java.jvmMode"=>"client", "TERM_PROGRAM_VERSION"=>"303", "SHLVL"=>"2", "NODE_PATH"=>"/usr/local/lib/node", "__CF_USER_TEXT_ENCODING"=>"0x5C8AC01D:0:0", "__array_start"=>"0", "SVN_EDITOR"=>"vim", "TERM_SESSION_ID"=>"C9A7D5A5-1296-47EA-9BAD-B924ADEEABE0", "rvm_prefix"=>"/Users/romel.campbell", "Apple_Ubiquity_Message"=>"/tmp/launch-X4HYZG/Apple_Ubiquity_Message", "TNS_ADMIN"=>"/usr/local/oracle/11.2/client64/network/admin", "LOGNAME"=>"romel.campbell", "IRBRC"=>"/Users/romel.campbell/.rvm/rubies/ruby-1.9.2-p320/.irbrc", "SQLPATH"=>"/usr/local/oracle/11.2/client64", "RUBY_VERSION"=>"ruby-1.9.2-p320", "_"=>"/Applications/eclipse/Eclipse.app/Contents/MacOS/eclipse", "SSH_AUTH_SOCK"=>"/tmp/launch-T9wk7T/Listeners", "LD_LIBRARY_PATH"=>"/usr/lib", "OLDPWD"=>"/Users/romel.campbell/RailsProjects/current/trunk", "SHELL"=>"/bin/bash", "rvm_bin_path"=>"/Users/romel.campbell/.rvm/bin", "TERM_SESSION_CLASS_ID"=>"5804EF65-F9C5-47D1-B6FC-2DCF3C38E5A7", "escape_flag"=>"1", "_first"=>"0", "_second"=>"1", "GEM_HOME"=>"/Users/romel.campbell/.rvm/gems/ruby-1.9.2-p320", "COMMAND_MODE"=>"unix2003", "DISPLAY"=>"/tmp/launch-lK9sGl/org.x:0", "USER"=>"romel.campbell", "com.apple.java.jvmTask"=>"JNI", "HOME"=>"/Users/romel.campbell", "LANG"=>"en_US.UTF-8"}
+  EOFError = EOFError
+  Encoding = Encoding
+  EncodingError = EncodingError
+  Enumerable = Enumerable
+  Enumerator = Enumerator
+  Errno = Errno
+  Etc = Etc
+  Exception = Exception
+  FALSE = false
+  FalseClass = FalseClass
+  Fiber = Fiber
+  FiberError = FiberError
+  File = File
+  FileTest = FileTest
+  FileUtils = FileUtils
+  Fixnum = Fixnum
+  Float = Float
+  FloatDomainError = FloatDomainError
+  GC = GC
+  Gem = Gem
+  Hash = Hash
+  IO = IO
+  IOError = IOError
+  IndexError = IndexError
+  Integer = Integer
+  Interrupt = Interrupt
+  Kernel = Kernel
+  KeyError = KeyError
+  LoadError = LoadError
+  LocalJumpError = LocalJumpError
+  Marshal = Marshal
+  MatchData = MatchData
+  Math = Math
+  Method = Method
+  Module = Module
+  Mutex = Mutex
+  NIL = nil
+  NameError = NameError
+  NilClass = NilClass
+  NoMemoryError = NoMemoryError
+  NoMethodError = NoMethodError
+  NotImplementedError = NotImplementedError
+  Numeric = Numeric
+  OUTPUT_PATH = "/Users/romel.campbell/Scala/workspace/.metadata/.plugins/com.aptana.ruby.core/-545838435/4/"
+  Object = Object
+  ObjectSpace = ObjectSpace
+  Proc = Proc
+  Process = Process
+  RUBY_COPYRIGHT = "ruby - Copyright (C) 1993-2012 Yukihiro Matsumoto"
+  RUBY_DESCRIPTION = "ruby 1.9.2p320 (2012-04-20 revision 35421) [x86_64-darwin11.3.0]"
+  RUBY_ENGINE = "ruby"
+  RUBY_PATCHLEVEL = 320
+  RUBY_PLATFORM = "x86_64-darwin11.3.0"
+  RUBY_RELEASE_DATE = "2012-04-20"
+  RUBY_REVISION = 35421
+  RUBY_VERSION = "1.9.2"
+  Random = Random
+  Range = Range
+  RangeError = RangeError
+  Rational = Rational
+  Regexp = Regexp
+  RegexpError = RegexpError
+  RubyVM = RubyVM
+  RuntimeError = RuntimeError
+  STDERR = IO.new
+  STDIN = IO.new
+  STDOUT = IO.new
+  ScriptError = ScriptError
+  SecurityError = SecurityError
+  Signal = Signal
+  SignalException = SignalException
+  StandardError = StandardError
+  StopIteration = StopIteration
+  String = String
+  Struct = Struct
+  Symbol = Symbol
+  SyntaxError = SyntaxError
+  SystemCallError = SystemCallError
+  SystemExit = SystemExit
+  SystemStackError = SystemStackError
+  TOPLEVEL_BINDING = #<Binding:0x007ff83b069d80>
+  TRUE = true
+  Thread = Thread
+  ThreadError = ThreadError
+  ThreadGroup = ThreadGroup
+  Time = Time
+  TrueClass = TrueClass
+  TypeError = TypeError
+  UnboundMethod = UnboundMethod
+  ZeroDivisionError = ZeroDivisionError
+
+
+
+  protected
+
+
+  private
+
+  def dir_names(arg0)
+  end
+
+  def file_name(arg0)
+  end
+
+  def get_classes
+  end
+
+  def grab_instance_method(arg0, arg1)
+  end
+
+  def print_args(arg0)
+  end
+
+  def print_instance_method(arg0, arg1)
+  end
+
+  def print_method(arg0, arg1, arg2, arg3, arg4, *rest)
+  end
+
+  def print_type(arg0)
+  end
+
+  def print_value(arg0)
+  end
+
+end
---
+++ workspace/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workspace::Application.config.secret_token = '68b2e909f0457df0205f2b50a3afd30c82e4407ae90ef51b9cfb51ebf90a27ad4cb306671835fd956e28f0ca59a1dea170367ae808c74ee5f896d29eea8210b8'
