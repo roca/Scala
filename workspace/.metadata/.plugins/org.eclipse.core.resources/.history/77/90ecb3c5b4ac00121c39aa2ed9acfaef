ea64d3ab6c5e7dd926a5479527833264151f1388
roca <RomelCampbell@gmail.com>
2013-04-24 04:11:07 -0400

    mergesort

---
+++ workspace/.metadata/.plugins/org.eclipse.core.resources/.history/77/90ecb3c5b4ac00121c39aa2ed9acfaef
@@ -0,0 +1,5 @@
+package week5
+
+object mergesort {
+  println("Welcome to the Scala worksheet")
+}
---
+++ workspace/progfun/.worksheet/src/week5.mergesort.scala
@@ -0,0 +1,24 @@
+package week5
+
+object mergesort {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(474); 
+  def msort(xs: List[Int]): List[Int] = {
+   val n = xs.length / 2
+   if (n ==0) xs
+   else {
+   	def merge(xs: List[Int],ys: List[Int]): List[Int] = (xs,ys) match {
+   	  case (Nil,ys) => ys
+   	  case (xs,Nil) => xs
+   	  case (x::xs1,y::ys1) => if (x < y) x :: merge(xs1,ys)
+   	  												else y :: merge(xs,ys1)
+   	                         
+   	
+   	}
+   	
+   	val (fst,snd) = xs splitAt n
+   	merge(msort(fst),msort(snd))
+   }
+  };System.out.println("""msort: (xs: List[Int])List[Int]""");$skip(33); 
+  
+  val nums = List(2,-4,5,7,1);System.out.println("""nums  : List[Int] = """ + $show(nums ));$skip(14); val res$0 = 
+  msort(nums);System.out.println("""res0: List[Int] = """ + $show(res$0))}
+}
