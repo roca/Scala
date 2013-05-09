85fbb5482cc0b31b3d7bba3fabee09135fdf98d2
roca <romelcampbell@gmail.com>
2013-05-09 14:17:58 -0400

    NoChange

--- workspace/forcomp/.worksheet/src/test.scala
+++ workspace/forcomp/.worksheet/src/test.scala
@@ -3,8 +3,11 @@ import forcomp.Anagrams._
 
 object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(80); 
 
-val sentence = List("Linux", "rulez");System.out.println("""sentence  : List[String] = """ + $show(sentence ));$skip(27); val res$0 = 
+val sentence = List("Linux", "rulez");System.out.println("""sentence  : List[String] = """ + $show(sentence ));$skip(29); val res$0 = 
+
+
 sentenceAnagrams(sentence);System.out.println("""res0: List[forcomp.Anagrams.Sentence] = """ + $show(res$0))}
+
 
 
 
