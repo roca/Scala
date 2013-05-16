ce0cfdce4b8ee053f9fa513e2d96f4aea9dda3c0
roca <RomelCampbell@gmail.com>
2013-05-15 21:16:11 -0400

    16:04

---
+++ workspace/.metadata/.plugins/org.eclipse.core.resources/.history/23/c0175faac2bd001217e3ad8b1d8c65d9
@@ -0,0 +1,5 @@
+package week7
+
+object test {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+}
---
+++ workspace/progfun/.worksheet/src/week7.test.scala
@@ -0,0 +1,6 @@
+package week7
+
+object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(71); 
+  val problem = new Pouring(Vector(4,7,9));System.out.println("""problem  : week7.Pouring = """ + $show(problem ));$skip(16); val res$0 = 
+  problem.moves;System.out.println("""res0: scala.collection.immutable.IndexedSeq[Product with Serializable with week7.test.problem.Move] = """ + $show(res$0))}
+}
